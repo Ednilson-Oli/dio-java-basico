f87a901bf3917c8a878dc344f652dd3129dcd6e9
Ednilson-Oli <ednilsonoliveira2@hotmail.com>
2023-09-02 20:40:16 -0300

    Uso do Scanner e tentando executar pelo cmd CLASS

---
+++ sistema-smart-TV/src/MinhaClasse.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MinhaClasse {
+    public static void main(String [] args){
+        System.out.println("ola");
+        Scanner scanner =new Scanner(System.in).useLocale(Locale.US);
+        String nome= scanner.next();
+
+    }
+}
